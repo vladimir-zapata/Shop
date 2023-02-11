f749d97c51d2a0c68e090ceb532dd8b213e8f676
DonySCode <32602581+DonySCode@users.noreply.github.com>
2023-02-11 18:40:45 -0400

    Adding model, controller and view.

---
+++ Shop.Web/Controllers/OrdersController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Web.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Shop.Web.Controllers
+{
+    public class OrdersController : Controller
+    {
+        // GET: OrdersController
+        public ActionResult Index()
+        {
+            List<OrdersModel> products = new List<OrdersModel>()
+            {
+                new OrdersModel()
+                {
+                    OrderID = 0001,
+                    CustID = 01,
+                    EmpID = 01,
+                    OrderDate = new DateTime(2023, 11, 02),
+                    ShippedDate = new DateTime(2023, 11, 09),
+                    ShipperID = 001,
+                    Freight = 199,
+                    ShipName = "AJ001",
+                    ShipAddress = "1798 ave 1st Apt 203",
+                    ShipCity = "Miami",
+                    ShipRegion = "Palm Beach County",
+                    ShipCountry = "United States",
+                    ShipPostalCode = 32129
+                }
+            };
+            return View();
+        }
+
+        // GET: OrdersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: OrdersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrdersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrdersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OrdersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrdersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OrdersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Shop.Web/Models/OrdersModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Shop.Web.Models
+{
+    public class OrdersModel
+    {
+        public int OrderID { get; set; }
+        public int CustID { get; set; }
+        public int EmpID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime ShippedDate { get; set; }
+        public int ShipperID { get; set; }
+        public int Freight { get; set; }
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipRegion { get; set; }
+        public int ShipPostalCode { get; set; }
+        public string ShipCountry { get; set; }
+    }
+    }
+
